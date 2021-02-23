c860bce66ff18a68bf6bd8ce8e7ecb189db3b4d2
chrisongthb <chri.s@mailbox.org>
2021-02-23 15:18:06 +0100

    Fix EmptyLineAfterFinalLet

--- spec/defines/client/configuration_item_spec.rb
+++ spec/defines/client/configuration_item_spec.rb
@@ -18,6 +18,7 @@ describe 'check_mk::client::configuration_item' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+
       it { is_expected.to compile.with_all_deps }
       it {
         is_expected.to contain_file('/etc/check_mk/myconfig.cfg').with(
