83723927a24bf0cf02541e9d17586ac108ab6f08
chrisongthb <chri.s@mailbox.org>
2021-02-23 10:25:38 +0100

    Resolve more style issues

--- lib/facter/omdsites.rb
+++ lib/facter/omdsites.rb
@@ -29,37 +29,33 @@ require 'facter'
 
 Facter.add('omdsites') do
   setcode do
-
     osfamily = Facter.value(:osfamily)
     case osfamily
-    when %r/Debian/
+    when %r{Debian}
       checkcommand = '/usr/bin/dpkg -l | /bin/grep check-mk-enterprise && /usr/bin/which omd'
-    when %r/RedHat/
+    when %r{RedHat}
       checkcommand = '/usr/bin/rpm -qa | /bin/grep check-mk-enterprise && /usr/bin/which omd'
     end
-
     unless Facter::Util::Resolution.exec(checkcommand).empty?
       omdstats = Facter::Util::Resolution.exec('/usr/bin/omd status -b')
       omd_hash = {}
       current_site_name = ''
       current_site_hash = {}
       omdstats.split("\n").each do |line|
-        if line =~ %r/^\[(.*)\]$/
-          if !current_site_hash.empty? and !current_site_name.empty?
+        if line =~ %r{^\[(.*)\]$}
+          if !current_site_hash.empty? && !current_site_name.empty?
             omd_hash[current_site_name.clone] = current_site_hash.clone
           end
           current_site_name = Regexp.last_match(1)
           current_site_hash = {}
-        elsif line =~ %r/([\w]+)\s+([\d]+)/
+        elsif line =~ %r{([\w]+)\s+([\d]+)}
           current_site_hash[Regexp.last_match(1)] = Regexp.last_match(2)
         end
       end
-      if !current_site_hash.empty? and !current_site_name.empty?
+      if !current_site_hash.empty? && !current_site_name.empty?
         omd_hash[current_site_name.clone] = current_site_hash.clone
       end
       omd_hash
     end
-
   end
-
 end
